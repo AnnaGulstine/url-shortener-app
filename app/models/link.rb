8503cb58057643e85992121f08613cb712a791ba
AnnaGulstine <annagulstine@gmail.com>
2016-05-29 20:31:44 -0500

    Added method to Link class to standardize the target url

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,4 +3,9 @@ class Link < ActiveRecord::Base
 
   validates :slug, presence: true
   validates :target_url, presence: true
+
+  def standardize_target_url!
+    target_url.gsub!("http://", "")
+    target_url.gsub!("https://", "")
+  end
 end
